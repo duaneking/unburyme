1fbe4a58ac44aa671fa5933b47757fcc5ac92721
Jordan Santell <jsantell@gmail.com>
2011-06-26 15:24:27 -0400

    Moving around build code

--- compiler/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Advanced Option, goddamn externs
-#java -jar compiler.jar --js=../src/Unburyme.js --js=../src/Config.js --js=../src/Date.js --js=../src/Graph.js --js=../src/interface.js --js=../src/LoanApp.js --js=../src/Loan.js --js=../src/ResultBar.js --js=../src/TotalResults.js --js_output_file=../build/unburyme.js --compilation_level=ADVANCED_OPTIMIZATIONS --externs jquery-1.5.externs.js --externs jquery.ui.widget.externs.js
-
-java -jar compiler.jar --js=../src/Unburyme.js --js=../src/Config.js --js=../src/Date.js --js=../src/Graph.js --js=../src/interface.js --js=../src/LoanApp.js --js=../src/Loan.js --js=../src/ResultBar.js --js=../src/TotalResults.js --js_output_file=../build/unburyme.js --compilation_level=SIMPLE_OPTIMIZATIONS
-
--- compiler/jquery.ui.widget.externs.js
+++
@@ -1,6 +0,0 @@
-/**
- * @param {string} name
- * @param {Object=} options
- * @return {jQueryObject}
- */
-jQuery.slider = function(name, options){};
